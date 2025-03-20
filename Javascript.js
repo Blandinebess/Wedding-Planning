74a396303cc8cf4e59672dd7ca645c8a26100590
Blandine Zongo <blandinebess7@gmail.com>
2025-03-20 00:35:17 -0400

    PracticeProject

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+// Show an alert when the "Learn More" button is clicked
+document.getElementById("learnMoreBtn").addEventListener("click", function () {
+  alert("Let us make My dream wedding a reality!");
+
+});
+document.getElementById('alertbutton').addEventListener('click', function() {
+    alert('for more detaail, contact the wedding planner!');
+});
